9c87553934363aa4d013aca16dc875ca75cdb6d5
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-12-24 20:31:19 +0600

    added MuiCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import "./App.css";
+// import MuiTypography from "./components/MuiTypography";
+// import MuiButton from "./components/MuiButton";
+// import MuiTextField from "./components/MuiTextField";
+// import MuiSelect from "./components/MuiSelect";
+// import MuiRadioButton from "./components/MuiRadioButton";
+// import MuiCheckbox from "./components/MuiCheckbox";
+// import MuiSwitch from "./components/MuiSwitch";
+// import MuiRating from "./components/MuiRating";
 // import MuiAutocomplete from "./components/MuiAutocomplete";
 // import MuiBoxLayout from "./components/MuiBoxLayout";
-// import MuiButton from "./components/MuiButton";
-// import MuiCheckbox from "./components/MuiCheckbox";
-// import MuiGridLayout from "./components/MuiGridLayout";
-import MuiPaperLayout from "./components/MuiPaperLayout";
-// import MuiRadioButton from "./components/MuiRadioButton";
-// import MuiRating from "./components/MuiRating";
-// import MuiSelect from "./components/MuiSelect";
 // import MuiStackLayout from "./components/MuiStackLayout";
-// import MuiSwitch from "./components/MuiSwitch";
-// import MuiTextField from "./components/MuiTextField";
-// import MuiTypography from "./components/MuiTypography";
+// import MuiGridLayout from "./components/MuiGridLayout";
+// import MuiPaperLayout from "./components/MuiPaperLayout";
+import MuiCard from "./components/MuiCard";
 
 const App = () => {
   return (
@@ -28,7 +29,8 @@ const App = () => {
       {/* <MuiBoxLayout /> */}
       {/* <MuiStackLayout /> */}
       {/* <MuiGridLayout /> */}
-      <MuiPaperLayout />
+      {/* <MuiPaperLayout /> */}
+      <MuiCard />
     </>
   );
 };
---
+++ src/components/MuiCard.jsx
@@ -0,0 +1,40 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+
+const MuiCard = () => {
+  return (
+    <Box width="300px">
+      <Card>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://source.unsplash.com/random"
+          alt="unsplash image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            React
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+            deleniti, corporis nesciunt rerum esse ipsa debitis iste tenetur
+            soluta possimus.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
+
+export default MuiCard;
